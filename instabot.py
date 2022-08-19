c6ab50dae5689aa45245c49d3fd714a258d053aa
ykratkovskiy <96204003+ykratkovskiy@users.noreply.github.com>
2022-08-19 14:45:19 +0300

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -8,28 +8,29 @@ import json
 import re
 from urllib.parse import unquote
 from list_of_exclusion_tags import badtags_set
-import time
+
+
+browser = webdriver.Chrome('instagram\chromedriver.exe')
 
 
 def instagram_login(username, password):
-    global browser
-    browser = webdriver.Chrome('instagram\chromedriver.exe')
+    
     try:
         browser.get('https://instagram.com')
-        time.sleep(random.randrange(2,3))
+        time.sleep(random.randrange(2,4))
 
         username_input = browser.find_element(By.NAME,'username')
         username_input.clear()
         username_input.send_keys(username)
-        time.sleep(random.randrange(1,2))
+        time.sleep(random.randrange(1,4))
 
         password_input = browser.find_element(By.NAME,'password')
         password_input.clear()
         password_input.send_keys(password)
-        time.sleep(random.randrange(1,2))
+        time.sleep(random.randrange(1,4))
 
         password_input.send_keys(Keys.ENTER)
-        time.sleep(random.randrange(4,6))
+        time.sleep(random.randrange(1,4))
     except Exception as ex:
         print (ex)
         browser.close()
@@ -44,14 +45,21 @@ def hashtaglike(hashtag):
         count_of_post_urls = 0
 
         browser.get(f'https://www.instagram.com/explore/tags/{hashtag}')
-        time.sleep(random.randrange(7,10))
+        time.sleep(random.randrange(5,9))
 
+        posts_urls = []
         for i in range (1,3):
-            browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-            time.sleep(random.randrange(27,37))
+            browser.execute_script('window.scrollBy(0, 1300);')
+            time.sleep(random.randrange(11,15))
             find_tags_a = browser.find_elements(By.TAG_NAME,'a')
-            posts_urls = {item.get_attribute('href') for item in find_tags_a if '/p/' in item.get_attribute('href')}
-                             
+            for item in find_tags_a:
+                post_href = item.get_attribute('href')
+                if '/p/' in post_href:
+                    posts_urls.append(post_href)
+                    
+        posts_urls = set(posts_urls)
+        posts_urls = list(posts_urls)
+                  
         for item in posts_urls:
             count_of_post_urls+=1
         print (f'Hashtag "{hashtag}" collected {count_of_post_urls} items in current session')
@@ -60,37 +68,38 @@ def hashtaglike(hashtag):
             json_file = json.load(js)
 
         start_time = time.time()
-        posts_urls = list(posts_urls)
-        for url in (posts_urls[1:count_of_post_urls]):
+        for url in (posts_urls[1:count_of_post_urls+1]):
             if url not in json_file:
                 json_file.append(url)
                 browser.get(url)
-                time.sleep(random.randrange(5,9))
-                
+                time.sleep(random.randrange(3,7))
                 
                 find_other_tags_a = browser.find_elements(By.TAG_NAME,'a')
                 unquoted_tag_set = {(unquote(item.get_attribute('href'))) for item in find_other_tags_a if '/explore/tags/' in (item.get_attribute('href'))}
                 tags = {(re.findall(r'\w+', item)[-1]) for item in unquoted_tag_set}
-                                
                 
+                                
                 if (tags.isdisjoint(badtags_set) == True):
                     like_button = browser.find_element(By.CSS_SELECTOR,'section:first-child span button').click()
                     counter_of_likes +=1
-                    time.sleep(random.randrange(121,130))
+                    print (f'Clicked {counter_of_likes} posts from {count_of_post_urls} ({(round(counter_of_likes/count_of_post_urls*100,1))}%)')
+                    time.sleep(random.randrange(180,192))
                 else:
                     time.sleep(random.randrange(1,3))
                     counter_of_exclusion_tags +=1
+                    print (f'{counter_of_exclusion_tags} tags passed')
                 
             else:
                 time.sleep(random.randrange(1,3))
                 counter_of_repeates+=1
+                print (f'Repeat! ({counter_of_repeates} for {hashtag})')
 
         
         with open ('instagram\json_list_of_liked_hrefs(countryside)', 'w') as lst:
             json.dump(json_file,lst)
         
         
-        print (f'Hashtag "{hashtag}" has been clicked {counter_of_likes} times in {round((time.time()-start_time)/60,1)} min')
+        print (f'Hashtag "{hashtag}" has been clicked {counter_of_likes} times in {round((time.time()-start_time)/60)} min')
         print (f'were occured {counter_of_repeates} repeated posts of {hashtag}')
         print (f'{counter_of_exclusion_tags} posts excluded by hashtag(s)')
                             
@@ -98,6 +107,44 @@ def hashtaglike(hashtag):
         print (ex)
         browser.close()
         browser.quit()
+
+
+def check_myfollowers():
+    browser.get(f'https://www.instagram.com/{username}/followers')
+    time.sleep(random.randrange(3,5))
+    
+    follower_ul = browser.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]')
+    follower_urls = []
+    followers_count = 0
+    
+    for i in range (0,13):
+        browser.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight',follower_ul)
+        time.sleep(random.randrange(3,5))
+        
+        find_tag_a = follower_ul.find_elements(By.TAG_NAME,'a')
+        for item in find_tag_a:
+            follower_href = item.get_attribute('href')
+            follower_urls.append(follower_href)
+    
+    follower_urls = set(follower_urls)
+   
+    for item in follower_urls:
+        followers_count +=1
+    print (f'У аккаунта "{username}" {followers_count} подписчиков')
+       
+ 
+    with open (f'instagram\json_list_of_followers({username})', 'r') as js_followers:
+        json_follower = json.load(js_followers)
+        json_follower = set(json_follower)
+    
+    difference = json_follower.difference(follower_urls)
+    if len(difference)!=0:
+        print (f'Из аккаунта "{username}" удалились(сменили id) следующие пользователи: {difference}')   
+    
+    follower_urls = list(follower_urls)
+    with open (f'instagram\json_list_of_followers({username})', 'w') as lst:
+        json.dump(follower_urls,lst)
+
 
 def browser_close():
     return browser.close(),browser.quit()
@@ -108,12 +155,9 @@ def pause():
 if __name__ == '__main__':
     instagram_login(username, password)
     pause()
-    hashtaglike('трамвай')
-    pause()
-    # hashtaglike('гродно2022')
-    # pause()
-    # hashtaglike('новогрудскийзамок')
-    # pause()
-    # hashtaglike('сула2022')
-    # pause()
+    # check_myfollowers()
+    
+    hashtaglike('турпобеларуси')
+   
+        
     browser_close()
